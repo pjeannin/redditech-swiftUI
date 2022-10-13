a6dd6fdf29e4a6858da1fe53c950ac11ec24b8c1
pjeannin <hebull.smash@gmail.com>
2022-10-12 18:43:28 -0700

    Initial Commit

---
+++ Redditech/RedditechApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedditechApp.swift
+//  Redditech
+//
+//  Created by Pierre Jeannin on 12/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct RedditechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
